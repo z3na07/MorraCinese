73934e847c8abb9bb0e91388e83f450ea13b2524
z3na07 <150442545+z3na07@users.noreply.github.com>
2023-12-10 15:00:52 +0100

    Add the elimination of the console and the start of the new choice

--- morraCinese/src/morra.java
+++ morraCinese/src/morra.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
 public class morra {
     public static void main(String[] args) {
         //dichiarazione variabili
-        char scelta, scelta1;
+        String scelta, scelta1;
+        //creazione Scanner
+        Scanner input= new Scanner(System.in);
+        //Presa in input dei valori
+        System.out.println("Inserire in base alla propria scelta: A per carta, S per sasso e D per forbici");
+        scelta= input.next();
+        //Pulizia console
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+     
 
     }
 }
